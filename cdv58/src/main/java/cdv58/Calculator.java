17eaaa84a7dd20cfabe89bba274aef4177f6dedf
Chaitra371 <chaitra0920@gmail.com>
2026-01-28 11:27:53 +0530

    intial commit

---
+++ cdv58/src/main/java/cdv58/Calculator.java
@@ -0,0 +1,23 @@
+package cdv58;
+
+public class Calculator {
+		public static int calculator(int a,int b,char op) {
+			if( op == '+') {
+				return a+b;
+			}
+			else {
+				return 0;
+			}
+		}
+
+		public static void main(String[] args) {
+			int a=10;
+			int b=20;
+			char op = '+';
+			int cal = calculator(a,b,op);
+			System.out.println(cal);
+
+		}
+
+	}
+
