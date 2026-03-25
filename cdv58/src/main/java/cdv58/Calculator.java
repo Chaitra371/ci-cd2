3887c77f6ca369c543c95651bc6da465ce560a30
Chaitra371 <chaitra0920@gmail.com>
2026-03-25 11:08:10 +0530

    Fix variable name in calculator method call

--- cdv58/src/main/java/cdv58/Calculator.java
+++ cdv58/src/main/java/cdv58/Calculator.java
@@ -1,24 +1,21 @@
 package cdv58;
 
 public class Calculator {
-		public static int calculator(int a,int b,char op) {
-			if( op == '+') {
-				return a+b;
-			}
-			else {
-				return 0;
-			}
-		}
+    public static int calculator(int a,int b,char op) {
+        if(op == '+') {
+            return a + b;
+        }
+        else {
+            return 0;
+        }
+    }
 
-		public static void main(String[] args) {
-			int ad=10;
-			int b=20;
-			char op = '+';
-			int cal = calculator(a,b,op);
-			System.out.println(cal);
-			System.out.println("Completed");
-
-		}
-
-	}
-
+    public static void main(String[] args) {
+        int ad = 10;
+        int b = 20;
+        char op = '+';
+        int cal = calculator(ad, b, op);  // FIXED
+        System.out.println(cal);
+        System.out.println("Completed");
+    }
+}
